50e7054da60ceb90e9daaaaf129f73cad744d2fb
ganeshnv <41773744+ganeshnv@users.noreply.github.com>
2019-04-05 16:38:54 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,40 @@
 #! python
 
 open('test.txt', 'w')
+
+
+python
+
+pip  -- tool to manage dependencies
+virtualenv   -- python library
+
+pip install virtualenv
+
+pyvenv myenv
+
+source myenv/bin/activate   --activate an environment
+
+pip install whois
+
+vim whois.py
+
+import whois
+w = whois.query('google.com')
+print(w.name)
+print(w.creation_date)
+print(w.name_servers)
+print(w.last_updated)
+
+python whois.py
+
+
+=======
+
+Network management -- netmiko library
+
+pip install netmiko
+
+input_data = input("Enter the IP address: ")
+
+Import getpass
+
